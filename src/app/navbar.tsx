b17a405ad3312e301c137fb8c0ac36717acce5da
laildev <92lailikumala@gmail.com>
2025-06-25 20:23:46 +0700

    add templete and navbar | remove some styling in global css | add config next | edit global page

---
+++ src/app/navbar.tsx
@@ -0,0 +1,38 @@
+"use client"
+
+import Link from 'next/link'
+import { usePathname, useRouter } from 'next/navigation'
+import React from 'react'
+
+const Navbar = () => {
+  const pathname = usePathname();
+  const router = useRouter();
+
+  return (
+    <nav className='flex bg-gray-800 py-2 px-5 justify-between'>
+      <div className='flex'>
+        <h1 className='text-white'>Navbar</h1>
+        <ul className="flex ml-5">
+          <Link href="/">
+            <li className={`mr-3 ${pathname === "/" ? "text-blue-300" : "text-white"} cursor-pointer`}>Home</li>
+          </Link>
+          <Link href="/about">
+            <li className={`mr-3 ${pathname === "/about" ? "text-blue-300" : "text-white"} cursor-pointer`}>About</li>
+          </Link>
+          <Link href="/about/profile">
+            <li className={`mr-3 ${pathname === "/about/profile" ? "text-blue-300" : "text-white"} cursor-pointer`}>Profile</li>
+          </Link>
+        </ul>
+      </div>
+      <div>
+        <button className='bg-white rounded-md px-3 text-sm h-7 cursor-pointer'
+          onClick={() => router.push("/login")}
+        >
+          login
+        </button>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ src/app/template.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Template = ({children} : {children: React.ReactNode}) => {
+
+  return (
+    <div>
+      {/* <h1>template</h1> */}
+      {children}
+    </div>
+  )
+}
+
+export default Template
